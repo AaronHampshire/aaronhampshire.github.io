27a400c5baf9a21c2ede1edc3a5b793b69f9fef4
Aaron Hampshire <aaron.hampshire@gmail.com>
2016-03-01 22:33:18 -0800

    Basic slider with style in modularized, modified SMACSS form

---
+++ scripts/slider.js
@@ -0,0 +1,24 @@
+/* Slider Toggle */
+$(".menu").click( function() {
+    if($(".site-wrap").hasClass("slide-right")) {
+        $(".site-wrap").toggleClass("slide-right");
+        $(".site-wrap,.darken,.menu").animate({
+            left: "-=250"
+            }, 300, function() {
+        });
+        $('.darken').fadeOut({queue: false});
+        $(".menu .top").toggleClass( "is-rotated-cw" );
+        $(".menu .middle").toggleClass( "is-transparent" );
+        $(".menu .bottom").toggleClass( "is-rotated-ccw" );
+    }
+    else {
+        $(".site-wrap").toggleClass("slide-right");
+        $(".site-wrap,.darken,.menu").animate({
+            left: "+=250" }, 300, function() {
+        });
+        $('.darken').fadeIn({queue: false});
+        $(".menu .top").toggleClass( "is-rotated-cw" );
+        $(".menu .middle").toggleClass( "is-transparent" );
+        $(".menu .bottom").toggleClass( "is-rotated-ccw" );
+    }
+});
