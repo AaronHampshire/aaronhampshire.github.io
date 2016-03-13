aaeb76c634b41707183e458928f9f8f1d9d545ba
Aaron Hampshire <aaron.hampshire@gmail.com>
2016-03-13 15:58:06 -0700

    Touch slider functionality

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,22 @@
-/* Slider Toggle */
 $(".menu").click( function() {
+    menuSlide();
+});
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $( ".site-wrap").on( "swiperight", menuSlide );
+    $(".darken").on( "swipeleft", menuSlide );
+}
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37:  case 39: // left & right
+            menuSlide();
+            break;
+        default: return;
+    }
+});
+
+function menuSlide (event) {
     if($(".site-wrap").hasClass("slide-right")) {
         $(".site-wrap").toggleClass("slide-right");
         $(".site-wrap,.darken").animate({
@@ -7,9 +24,7 @@ $(".menu").click( function() {
             }, 300, function() {
         });
         $('.darken').fadeOut({queue: false});
-        $(".menu .top").toggleClass( "is-rotated-cw" );
-        $(".menu .middle").toggleClass( "is-transparent" );
-        $(".menu .bottom").toggleClass( "is-rotated-ccw" );
+        menuTransform();
     }
     else {
         $(".site-wrap").toggleClass("slide-right");
@@ -17,8 +32,12 @@ $(".menu").click( function() {
             left: "+=250" }, 300, function() {
         });
         $('.darken').fadeIn({queue: false});
+        menuTransform();
+    }
+}
+
+function menuTransform () {
         $(".menu .top").toggleClass( "is-rotated-cw" );
         $(".menu .middle").toggleClass( "is-transparent" );
         $(".menu .bottom").toggleClass( "is-rotated-ccw" );
-    }
-});
+}
